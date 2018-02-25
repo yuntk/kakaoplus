c53bd56e999305ad7720bf079d42bd54c5db19a5
yuntk <semf616@gmail.com>
2018-02-25 19:22:08 +0900

    Add post mathod respoding to user massage

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* kakao plus friend main menu */
 router.get('/keyboard', (req,res,next)=>{
   let obj = {
     "type":"text"
+  }
+  res.send(obj)
+})
+
+/**
+   * If kakao user sended message in plus friend menu,
+   * kakao server post request to {app address registerd by me}/message
+   * The req.body Data is JSON format like below
+   * {
+   *   "user_key": "encryptedUserKey",
+   *   "type": "text",
+   *   "content": "Sended message"
+   * }
+   * or
+   * {
+   *    "user_key": "encryptedUserKey",
+   *    "type": "photo",
+   *    "content": "http://photo_url/number.jpg"
+   * }
+   */
+router.post('/massage', (req,res,next)=>{
+  let obj = {
+    "message":{
+        "text" : req.body.content
+   }
   }
   res.send(obj)
 })
