ee27c6864968a7e8995117d7430565489bfc9adf
yuntk <semf616@gmail.com>
2018-02-26 20:20:53 +0900

    ckpt upload

--- db.sql
+++
@@ -1,4 +0,0 @@
-create table userstyle (
-	userkey varchar(30) primary key,
-	style INT(11) null default 1
-)
--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,9 @@ var pool  = mysql.createPool({
 /* kakao plus friend main menu */
 router.get('/keyboard', (req,res,next)=>{
   let obj = {
-    "message":{
-      "text" : "아래 버튼에서 원하는 스타일을 선택해주세요"
-    },
     "keyboard" : {
       "type" : "buttons",
       "buttons" : [
-        "그림 보기",
         "1.Rain Princesss, by Leonid Afremov",
         "2.La Muse, by Pablo Picasso",
         "3.Udnie by Francis Picabia",
@@ -53,9 +49,9 @@ router.post('/message', (req,res,next)=>{
   var userkey = req.body.user_key;
   var upurl = req.body.content;
   var downurl = "http://13.124.76.13:3001/images/"+userkey+"out.jpg"
-  var sql = "insert into imagelist(userkey,upurl,downurl) values(?,?,?)";
-  var arr = [userkey,upurl,downurl]
   if(req.body.type=="photo"){
+    var sql = "insert into imagelist(userkey,upurl,downurl) values(?,?,?)";
+    var arr = [userkey,upurl,downurl];
     pool.getConnection((err, conn) => {
       if(err) { console.log(err); return; }
       conn.query(sql, arr, (err, result)=>{
@@ -71,27 +67,7 @@ router.post('/message', (req,res,next)=>{
     });  
   }else{
     msg = req.body.content;
-    if(msg == "그림 보기"){
-      var obj = {
-        "message":{
-          "text" : "위에서부터 1번 그림입니다.",
-          
-        },
-        "keyboard" : {
-          "type" : "buttons",
-          "buttons" : [
-            "그림 보기",
-            "1.Rain Princesss, by Leonid Afremov",
-            "2.La Muse, by Pablo Picasso",
-            "3.Udnie by Francis Picabia",
-            "4.Scream, by Edvard Munch",
-            "5.The Great Wave off Kanagawa, by Hokusai",
-            "6.The Shipwreck of the Minotaur, by J.M.W. Turner"
-          ]
-        }
-      }
-      res.send(obj)
-    }else if(msg == "1.Rain Princesss, by Leonid Afremov"
+    if(msg == "1.Rain Princesss, by Leonid Afremov"
       || msg == "2.La Muse, by Pablo Picasso"
       || msg == "3.Udnie by Francis Picabia"
       || msg == "4.Scream, by Edvard Munch"
@@ -99,7 +75,7 @@ router.post('/message', (req,res,next)=>{
       || msg == "6.The Shipwreck of the Minotaur, by J.M.W. Turner"){
       var obj = {
         "message":{
-          "text" : "사진을 올려주세요. 다른 스타일을 원하면 아무 메세지나 입력하세요.",
+          "text" : msg+" 을 선택하셨습니다.\n 아래 그림과 같은 스타일로 변환하고 싶은 사진을 + 버튼을 눌러서 업로드 해주세요.\n 만약 다른 스타일을 원하면 아무 메세지나 입력하세요.",
           "photo": {
             "url": "http://13.124.76.13:3001/images/paint"+msg[0]+".png",
             "width": 640,
@@ -107,6 +83,15 @@ router.post('/message', (req,res,next)=>{
           }
         }
       }
+      var sql = "INSERT INTO userstyle(userkey,style) VALUES (?, ?) ON DUPLICATE KEY UPDATE userkey=?, style=?";
+      var arr = [userkey,msg[0],userkey,msg[0]];
+      pool.getConnection((err, conn) => {
+        if(err) { console.log(err); return; }
+        conn.query(sql, arr, (err, result)=>{
+          conn.release();
+          if(err) { console.log(err); return; }
+        });
+      });  
       res.send(obj)
     }else{
       let obj = {
@@ -116,7 +101,6 @@ router.post('/message', (req,res,next)=>{
         "keyboard" : {
           "type" : "buttons",
           "buttons" : [
-            "그림 보기",
             "1.Rain Princesss, by Leonid Afremov",
             "2.La Muse, by Pablo Picasso",
             "3.Udnie by Francis Picabia",
